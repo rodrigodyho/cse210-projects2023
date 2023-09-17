427e8c52e2dd1a50c77c75952ca148eb58ead44b
Rodrigo Lima <rodrigodyho@gmail.com>
2023-09-17 18:03:05 -0300

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,57 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Random randomGenerator = new Random();
+    bool playAgain = true;
+    int guess;
+    int magicNumber;
+    int numberOfGuesses;
+    string answer;
+
+    while (playAgain)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      guess = 0;
+      numberOfGuesses = 0;
+      answer = "";
+      magicNumber = randomGenerator.Next(1, 101);
+
+      while (guess != magicNumber)
+      {
+        Console.WriteLine("Guess a number between 1 and 100: ");
+        guess = int.Parse(Console.ReadLine());
+        Console.WriteLine("You chose the number: " + guess);
+
+        if (guess > magicNumber)
+        {
+          Console.WriteLine(guess + ": this number is too high!");
+        }
+        else if (guess < magicNumber)
+        {
+          Console.WriteLine("The number " + guess + " is too low!");
+        }
+        numberOfGuesses++;
+      }
+      Console.WriteLine("");
+      Console.WriteLine("Magic Number: " + magicNumber);
+      Console.WriteLine("YOU WIN!");
+      Console.WriteLine("Number of Guesses: " + numberOfGuesses);
+
+      Console.WriteLine("Do you want to play again (y/n): ");
+      answer = Console.ReadLine();
+
+      if (answer == "y")
+      {
+        playAgain = true;
+      }
+      else
+      {
+        playAgain = false;
+      }
     }
+
+    Console.WriteLine("Thanks for playing!");
+
+  }
 }

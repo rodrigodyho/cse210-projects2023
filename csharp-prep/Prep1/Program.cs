677f3314b79f416cceaa8a12a1ad693d238166b0
Rodrigo Lima <rodrigodyho@gmail.com>
2023-09-17 17:49:26 -0300

    Learning Activity - C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+//it just display a prompt and receive information.
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("What is your first name?");
+    string firstName = Console.ReadLine();
+
+    Console.WriteLine("What is your last name?");
+    string lastName = Console.ReadLine();
+
+    Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+  }
+  
 }
